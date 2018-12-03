7b0fd53ad5a9b1e4458eefcae1e7a565dda8cff3
Adil khan <adkhan1992@gmail.com>
2018-12-03 18:16:20 +0530

    get user current location

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import { ScrollView, StyleSheet , View} from 'react-native';
+import { ScrollView, StyleSheet, View, Platform } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import MapView from 'react-native-maps';
+import { Constants, Location, Permissions } from 'expo';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
+  state = {
+    location: null,
+    errorMessage: null,
+  };
 
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    console.log("location", location);
+    this.setState({ location });
+  };
   render() {
 
     return (
@@ -15,7 +42,7 @@ export default class LinksScreen extends React.Component {
       <View style={styles.container}>
         <MapView
           style={styles.map}
-          initialRegion={{ // initial region set to Bileto
+          initialRegion={{
             latitude: 50.0517273,
             longitude: 14.4286503,
             latitudeDelta: 0.0922,
