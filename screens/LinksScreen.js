6a156c2176badfbfc932b2eb21cdffa9de39411a
Adil khan <adkhan1992@gmail.com>
2018-12-03 16:42:07 +0530

    setup

---
+++ screens/LinksScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { ExpoLinksView } from '@expo/samples';
+import MapView from 'react-native-maps';
+
+export default class LinksScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Links',
+  };
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
