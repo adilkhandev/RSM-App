972a869769610addabe2e7d9139af88c8027d051
Adil khan <adkhan1992@gmail.com>
2018-12-03 21:01:44 +0530

    Add more custom marker and user location

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -12,9 +12,33 @@ export default class LinksScreen extends React.Component {
     location: null,
     errorMessage: null,
     markers: [{
-      latlng: { "latitude": 28.6931858, "longitude": 77.1501815 },
-      title: "adil",
-      description: "dadsdas"
+      latlng: { "latitude": 28.716499, "longitude": 77.170505 },
+      title: "Chrysler Group Llc",
+      description: "Manufactures motor vehicles & car bodies"
+    }, {
+      latlng: { "latitude": 28.566828, "longitude": 77.279372 },
+      title: "Metal heat treating nsk",
+      description: "Metal Heat Treating"
+    }, {
+      latlng: { "latitude": 28.385961, "longitude": 77.31343 },
+      title: "30893 Century Drive",
+      description: "Professional equipment nec nsk"
+    }, {
+      latlng: { "latitude": 28.56404, "longitude": 77.334269 },
+      title: "Combi Packaging Systems Llc",
+      description: "Packaging Machinery Manufacturing"
+    }, {
+      latlng: { "latitude": 28.552322, "longitude": 77.033463 },
+      title: "Distinctive Mfg Group Llc",
+      description: "General industrial machinery nec nsk"
+    }, {
+      latlng: { "latitude": 28.729923, "longitude": 77.10222 },
+      title: "Aaf Flanders Inc.",
+      description: "I forwarded information that the customer requested."
+    }, {
+      latlng: { "latitude": 28.548833, "longitude": 77.120696 },
+      title: "Cameron Diversified Products",
+      description: "Rolled Steel Shape Manufacturing"
     }]
   };
 
@@ -37,11 +61,11 @@ export default class LinksScreen extends React.Component {
     }
 
     let location = await Location.getCurrentPositionAsync({});
-    console.log("location", location);
+    console.log("666", location);
     this.setState({ location });
   };
   render() {
-   return (
+    return (
 
       <View style={styles.container}>
         <MapView
@@ -53,11 +77,16 @@ export default class LinksScreen extends React.Component {
             longitudeDelta: 0.02
           }}
         >
+          <Marker
+            coordinate={{ "latitude": 28.6931858, "longitude": 77.1501815 }}
+            image={require('../assets/images/location.png')}
+            
+          />
           {this.state.markers.map((marker) => {
             return (
               <Marker
                 key={marker.title}
-              
+
                 coordinate={marker.latlng}
                 title={marker.title}
                 image={require('../assets/images/light-green.png')}
